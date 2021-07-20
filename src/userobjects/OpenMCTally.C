f4e90bc1e1e980f123e771f3d27a6bf0eaaddb07
Robert Crowder <robert.crowder@inl.gov>
2021-07-20 09:59:13 -0400

    Tally and filter ids now required

--- src/userobjects/OpenMCTally.C
+++ src/userobjects/OpenMCTally.C
@@ -14,7 +14,6 @@
 #include "openmc/tallies/filter_energy.h"
 #include "openmc/tallies/filter_particle.h"
 #include "openmc/tallies/filter_universe.h"
-#include "openmc/tallies/filter_cell.h"
 #include "openmc/particle_data.h"
 
 #include <string>
@@ -31,10 +30,12 @@ OpenMCTally::validParams()
   MooseEnum estimator_types("ANALOG TRACKLENGTH COLLISION", "COLLISION");
   MooseEnum particle_types("neutron photon electron positron", "neutron");
 
+  params.addRequiredParam<int>("tally_id", "Tally id used to extract tally from an auxkernal")
   params.addParam<MooseEnum>("particle_type", particle_types,"particle type to track in tally");
   params.addParam<MooseEnum>("tally_estimator", estimator_types, "Estimator type used for the tally");
   params.addRequiredParam<std::vector<std::string>>("tally_scores","Scores to apply to the tally");
   params.addRequiredParam<std::vector<std::string>>("tally_filters","Filters to apply to the tally");
+  params.addRequiredParam<std::vector<int>>("filter_ids","Filter ids used to extract tally from an auxkernal")
   params.addParam<std::vector<Real>>("tally_energy_bins","Define energy bins for the tally");
 
   return params;
@@ -42,10 +43,12 @@ OpenMCTally::validParams()
 
 OpenMCTally::OpenMCTally(const InputParameters & params)
   : GeneralUserObject(params),
+  _tally_id(getParam<int>("tally_id"))''
   _tally_particle(getParam<MooseEnum>("particle_type")),
   _tally_estimator(params.get<MooseEnum>("tally_estimator")),
   _tally_scores(getParam<std::vector<std::string>>("tally_scores")),
   _tally_filters(getParam<std::vector<std::string>>("tally_filters")),
+  _filter_ids(getParam<std::vector<int>>("filter_ids")),
   _tally_energy_bins(getParam<std::vector<Real>>("tally_energy_bins"))
 {
   if (_tally_particle != 0) {
@@ -59,12 +62,6 @@ OpenMCTally::OpenMCTally(const InputParameters & params)
     paramError("execute_on", "execute_on must be INITIAL to ensure tallies are created "
      "once at the beginning of the simulation");
   }
-
-  //if (_tally_filters.contains("energy") && _tally_energy_bins.empty()) {
-  //  paramError("_tally_energy_bins", "Energy bins are required when using energy filters");
-  //}
-
-
 }
 
 void
@@ -73,8 +70,8 @@ OpenMCTally::initialize()
   using namespace openmc;
 
   // create a new tally with auto id
-  _console << "Creating new tally" << std::endl;
-  model::tallies.push_back(make_unique<Tally>(C_NONE));
+  std::cout << "Creating new tally" << std::endl;
+  model::tallies.push_back(make_unique<Tally>(_tally_id));
 
   // TODO: check for a mesh parameter and add mesh if exists
 
@@ -89,7 +86,7 @@ OpenMCTally::initialize()
     // make filter param MooseEnum?
     // make method to get string given enum to git rid of if/switch statments?
 
-    Filter* filter_ptr = Filter::create(_tally_filters.at(i), C_NONE);
+    Filter* filter_ptr = Filter::create(_tally_filters.at(i), _filter_ids.at(i));
 
     if (filter_ptr->type() == "energy" ){
       _console << " Adding energy filter" << std::endl;
@@ -117,18 +114,9 @@ OpenMCTally::initialize()
 
       universe_filter->set_universes(universe_ids);
 
-    } else if (filter_ptr->type() == "cell"){
-      std::cout << " Adding cell filter" << std::endl;
+    }
 
-      CellFilter* cell_filter = dynamic_cast<CellFilter*>(filter_ptr);
-
-      vector<int> cell_ids;
-      for(int i = 0; i < model::cells.size(); ++i)
-        cell_ids.push_back(i);
-
-      cell_filter->set_cells(cell_ids);
-
-    } else
+    else
       mooseError("Unrecognized filter");
 
     filters.push_back(filter_ptr);
@@ -138,7 +126,7 @@ OpenMCTally::initialize()
 
   // apply scores
   _console << "Adding tally scores" << std::endl;
-  // model::tallies.back()->set_scores(_tally_scores);
+  model::tallies.back()->set_scores(_tally_scores);
 
   // set the tally estimator
   // get rid of switch with enum to string method?
@@ -170,7 +158,6 @@ OpenMCTally::initialize()
     t->init_results();
   }
 
-  _console << "Tally id: " << model::tallies.back()->id_ << std::endl << std::endl;
 }
 
 void
